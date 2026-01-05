5053d4060f532588149ac8f8d37a6db28ea0fee1
Cs Bence <bcsaldi18@gmail.com>
2026-01-05 13:44:48 +0100

    Initial Commit

---
+++ NewYearNewMe/NewYearNewMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewYearNewMeApp.swift
+//  NewYearNewMe
+//
+//  Created by Bence Családi on 2026. 01. 05..
+//
+
+import SwiftUI
+
+@main
+struct NewYearNewMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
